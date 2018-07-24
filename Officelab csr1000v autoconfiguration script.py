9d5aaab9057afbcbd08fedd48ea12f83074689ee
Billy <billy0529@hotmail.com>
2018-07-24 15:36:30 +0800

    Update file list add cisco telnet auto config script 180723

---
+++ Officelab csr1000v autoconfiguration script.py
@@ -0,0 +1,39 @@
+import sys
+import getpass
+import telnetlib
+
+IPRANGE = "172.1.1."
+
+user = input("Please Enter valid username: ")
+password = getpass.getpass()
+
+DEVICE = 0
+while DEVICE < 20:
+    DEVICE = DEVICE + 1
+    HOST = IPRANGE + str(DEVICE)
+    tn = telnetlib.Telnet(HOST)
+
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+
+    tn.write(b"conf t\n")
+    tn.write(b"username billy pri 15 pass gkswodls61\n")
+    tn.write(b"no ip domain-lookup\n")
+    tn.write(b"service password-encryption\n")
+    tn.write(b"interface g5\n")
+    tn.write(b"des [MANAGEMENT IP ASSIGNED]\n")
+    tn.write(b"exit\n")
+    tn.write(b"line vty 0 4\n")
+    tn.write(b"login local\n")
+    tn.write(b"logg sync\n")
+    tn.write(b"exec-time 0 0\n")
+    tn.write(b"end\n")
+    tn.write(b"wr\n")
+    tn.write(b"exit\n")
+
+    print(tn.read_all().decode('ascii'))
+    if DEVICE == 20:
+        print("=== Remote Access Configuration Done! ===")
