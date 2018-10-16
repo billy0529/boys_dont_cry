587074504ec90ae2b59666d9bee685248634df16
Billy <billy0529@hotmail.com>
2018-10-16 18:51:59 +0800

    Commit 181016

--- Officelab csr1000v autoconfiguration script.py
+++ Officelab csr1000v autoconfiguration script.py
@@ -3,6 +3,7 @@ import getpass
 import telnetlib
 
 IPRANGE = "172.1.1."
+IPRANGE = "192.168.90." 
 
 user = input("Please Enter valid username: ")
 password = getpass.getpass()
@@ -21,19 +22,26 @@ while DEVICE < 20:
 
     tn.write(b"conf t\n")
     tn.write(b"username billy pri 15 pass gkswodls61\n")
+    tn.write(b"configuration terminal\n")
+    tn.write(b"username billy privilage 15 password gkswodls61\n")
     tn.write(b"no ip domain-lookup\n")
     tn.write(b"service password-encryption\n")
     tn.write(b"interface g5\n")
     tn.write(b"des [MANAGEMENT IP ASSIGNED]\n")
+    tn.write(b"description [MANAGEMENT IP ASSIGNED]\n")
     tn.write(b"exit\n")
     tn.write(b"line vty 0 4\n")
     tn.write(b"login local\n")
     tn.write(b"logg sync\n")
     tn.write(b"exec-time 0 0\n")
+    tn.write(b"logging synchronous\n")
+    tn.write(b"exec-timeout 0 0\n")
     tn.write(b"end\n")
     tn.write(b"wr\n")
+    tn.write(b"write memory\n")
     tn.write(b"exit\n")
 
     print(tn.read_all().decode('ascii'))
     if DEVICE == 20:
+        print("=== Remote Access Configuration Done! ===")
         print("=== Remote Access Configuration Done! ===")
