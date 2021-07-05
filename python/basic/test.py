4c50f725c63c9869c37c2f7481c43f6a39fdbd03
Billy <billy0529@hotmail.com>
2021-07-05 17:36:43 +0800

    commit

--- python/basic/test.py
+++ python/basic/test.py
@@ -1,6 +1,12 @@
-import sys
+import random
 
-print("THIS AGE CALCULATOR")
-name_a = input("Name: ")
-bday_a = input("Birthday(yymmdd): ")
-date_a = input("Current Date(yymmdd): ")
+def rannum():
+    number = random.randint(100,300)
+    return number
+count = 0
+emlist = []
+while count <= 10:
+    ran_number = rannum()
+    emlist.append(ran_number)
+    count = count + 1
+print(emlist)
