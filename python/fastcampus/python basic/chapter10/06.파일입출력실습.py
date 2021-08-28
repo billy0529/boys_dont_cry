b8c526820c0215bd46dfec9e42cbfd019def6601
Billy <billy0529@hotmail.com>
2021-08-28 22:30:38 +0800

    commit

--- python/fastcampus/python basic/chapter10/06.파일입출력실습.py
+++ python/fastcampus/python basic/chapter10/06.파일입출력실습.py
@@ -4,52 +4,25 @@
 # 수익금 = (목표가 - 매입가) * 수량
 # 수익률 = (목표가/매입가 -1) * 100
 
-
 import csv
 
-data = [
-    "종목", "매입가", "수량", "목표가",
-    "삼성전자", "85000", "10", "90000",
-    "NAVER", "380000", "5", "400000",
-    "LG", "30000", "20", "53000"
-]
+def show_profit(data):
+    name = data[0]
+    purchase = int(data[1])
+    amount = int(data[2])
+    target_price = int(data[3])
+    
+    profit = (target_price - purchase) * amount
+    profit_ratio = ((target_price / purchase - 1) * 100)
+    print("{} {} {}%".format(name, profit, round(profit_ratio, 2)))
 
-file = open("mystock.csv", "w", encoding="utf-8-sig")
-writer = csv.writer(file)
+# 파일 열기
+file = open("mystock.csv", "r")
 
-for d in data:
-    writer.writerow(d)
-
-file = open("mystock.csv","r", encoding="utf-8-sig")
 reader = list(csv.reader(file))
+for data in reader[1:]:
+    show_profit(data)    
+
+
 file.close()
 
-
-# class Bene_margin:
-# 
-#     def __init__(self, row):
-#         self.name = data[row][0]
-#         self.purchase = data[row][1]
-#         self.qty = data[row][2]
-#         self.target = data[row][3]
-#     
-#     def benefit(self):
-#         return (self.target - self.purchase) * self.qty
-#         # 수익금 = (목표가 - 매입가) * 수량
-# 
-#     def margin(self):
-#         return round(((self.target / self.purchase - 1 ) * 100), 2) 
-#         # 소수점 2자리까지
-#         # 수익률 = (목표가/매입가 -1) * 100
-# 
-#     def data_name(self):
-#         return self.name
-#         # 종목이름
-# 
-# samsung = Bene_margin(1)
-# print(samsung.data_name(), samsung.benefit(), samsung.margin())
-# naver = Bene_margin(2)
-# print(naver.data_name(), naver.benefit(), naver.margin())
-# lg = Bene_margin(3)
-# print(lg.data_name(), lg.benefit(), lg.margin())
-# 
